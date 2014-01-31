c1b4adf915ee565478a9dbb351f87c65d7de764c
adamhammes <adamhammes@centetune.com>
2014-01-30 20:23:15 -0600

    Added calculate_color() prototype

--- pixel_functions.h
+++ pixel_functions.h
@@ -1,6 +1,9 @@
 #include "pixel.h"
 
 void black_and_white(Pixel *p);
-void color_shift(Pixel *p, char *pattern);
-void overlay(Pixel *a, Pixel *b);
 
+unsigned char calculate_color(unsigned char r1, unsigned char r2, unsigned char a1, unsigned char a2);
+
+void color_shift(Pixel *p, char *pattern);
+
+void overlay(Pixel *a, Pixel *b);
