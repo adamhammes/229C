2ccb16fdc16918948a7ce5e50f8d52b36d2e6fef
adamhammes <adamhammes@centetune.com>
2014-01-30 20:31:05 -0600

    Got rid of white space

--- pixel_functions.h
+++ pixel_functions.h
@@ -1,9 +1,6 @@
 #include "pixel.h"
 
 void black_and_white(Pixel *p);
-
 unsigned char calculate_color(unsigned char r1, unsigned char r2, unsigned char a1, unsigned char a2);
-
 void color_shift(Pixel *p, char *pattern);
-
 void overlay(Pixel *a, Pixel *b);
