330ce977a3caf87e3893d800ea669ce3298abdf3
adamhammes <adamhammes@centetune.com>
2014-02-03 12:15:27 -0600

    Added definition for create_pixel()

--- pixel_functions.h
+++ pixel_functions.h
@@ -1,5 +1,6 @@
 #include "pixel.h"
 
+Pixel create_pixel(unsigned char r, unsigned char g, unsigned char b, unsigned char a);
 void black_and_white(Pixel *p);
 unsigned char calculate_color(unsigned char r1, unsigned char r2, unsigned char a1, unsigned char a2);
 void color_shift(Pixel *p, char *pattern);
