d5a9d04ecd87e3f85d6ae7a19e201a4847247a26
adamhammes <adamhammes@centetune.com>
2014-02-05 12:34:30 -0600

    Switched #include to be more... inclusive!

--- pixel_functions.h
+++ pixel_functions.h
@@ -1,7 +1,7 @@
 #include "pixel.h"
 #include <String.h>
 
-Pixel create_pixel(unsigned char r, unsigned char g, unsigned char b, unsigned char a);
+Pixel* create_pixel(unsigned char r, unsigned char g, unsigned char b, unsigned char a);
 void black_and_white(Pixel *p);
 unsigned char calculate_color(unsigned char r1, unsigned char r2, unsigned char a1, unsigned char a2);
 void color_shift(Pixel *p, char* pattern);
