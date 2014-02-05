9e31428a9c0ad884481534adc874ef3a17ac4b28
adamhammes <adamhammes@centetune.com>
2014-02-05 12:43:57 -0600

    Created grey_test() and shell for main method

--- pixel_test.c
+++ pixel_test.c
@@ -1,5 +1,37 @@
 #include "pixel_test.h"
 
+
+int main(int argc, char** argv) {
+	return 0;
+}
+
+
+int creation_test() {
+	Pixel a, b;
+
+	a.red = 50;
+	a.blue = 100;
+	a.green = 150;
+	a.alpha = 200;
+
+	b = *create_pixel(50, 100, 150, 200);
+
+	return pixel_equals(&a, &b);
+}
+
+int grey_test() {
+	Pixel a, b;
+
+	a = *create_pixel(50, 100, 150, 200);
+	black_and_white(&a);
+
+	b.red = b.blue = b.green = 100;
+	b.alpha = 200;
+
+	return pixel_equals(&a, &b);
+}
+
+
 int pixel_equals(Pixel *a, Pixel *b) {
 	return	a->red == b->red &&
 			a->blue == b->blue &&
