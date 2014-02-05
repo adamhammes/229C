e221e07f25f1f9f26d9057924d77140667160076
adamhammes <adamhammes@centetune.com>
2014-02-05 14:25:43 -0600

    Added overlay_test()

--- pixel_test.c
+++ pixel_test.c
@@ -24,10 +24,22 @@ int grey_test() {
 
 	a = *create_pixel(50, 100, 150, 100);
 	black_and_white(&a);
-
+	/* b has correct values */
 	b.red = b.blue = b.green = b.alpha = 100;
 
 	return pixel_equals(&a, &b);
+}
+
+int overlay_test() {
+	Pixel a, b, c;
+
+	a = *create_pixel(50, 100, 150, 200);
+	b = *create_pixel(200, 150, 100, 50);
+
+	overlay(&a, &b);
+	c = *create_pixel(70, 92, 114, 210);
+
+	return pixel_equals(&a, &c);
 }
 
 
