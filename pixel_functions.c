222886d2c440b6bf4e63907bfdf9f51d150065aa
adamhammes <adamhammes@centetune.com>
2014-02-07 11:42:44 -0600

    Switched to lround() instead of round()

--- pixel_functions.c
+++ pixel_functions.c
@@ -25,7 +25,7 @@ void overlay(Pixel *a, Pixel *b) {
 	temp.red =   calculate_color(a->red, b->red, a->alpha, b->alpha);
 	temp.blue =  calculate_color(a->blue, b->blue, a->alpha, b->alpha);
 	temp.green = calculate_color(a->green, b->green, a->alpha, b->alpha);
-	temp.alpha = (unsigned char) round( (double) b->alpha + a->alpha * (255- b->alpha) / 255 );
+	temp.alpha = (unsigned char) lround( (double) b->alpha + a->alpha * (255- b->alpha) / 255 );
 
 	*a = temp;
 }
@@ -33,7 +33,7 @@ void overlay(Pixel *a, Pixel *b) {
 unsigned char calculate_color(unsigned char r1, unsigned char r2, unsigned char a1, unsigned char a2) {
 	double d_a2 = (double) a2; /* force double division in calculation */
 	
-	return (unsigned char) round( (d_a2 * r2 /255) + a1* (255-d_a2) *r1 / (255* 255));
+	return (unsigned char) lround( (d_a2 * r2 /255) + a1* (255-d_a2) *r1 / (255* 255));
 }
 
 void color_shift(Pixel *p, char* pattern) {
