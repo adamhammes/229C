1e3e11237036ede457895855cdd570217e77e2d0
adamhammes <adamhammes@centetune.com>
2014-02-20 07:34:42 -0600

    Overlay now returns an actual Pixel

--- pixel_functions.h
+++ pixel_functions.h
@@ -5,6 +5,6 @@ Pixel create_pixel(unsigned char r, unsigned char g, unsigned char b, unsigned c
 void black_and_white(Pixel *p);
 unsigned char calculate_color(unsigned char r1, unsigned char r2, unsigned char a1, unsigned char a2);
 void color_shift(Pixel *p, char* pattern);
-void overlay(Pixel *a, Pixel *b);
+Pixel overlay(Pixel *a, Pixel *b);
 void swap_colors(unsigned char *a, unsigned char *b);
 void triple_swap( unsigned char *a, unsigned char *b, unsigned char *c);
