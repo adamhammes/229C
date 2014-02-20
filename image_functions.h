feb8e00e66c183f27ccd4324996ad143bcfa6483
adamhammes <adamhammes@centetune.com>
2014-02-20 07:32:09 -0600

    Added definitions for overlay and make_image()

--- image_functions.h
+++ image_functions.h
@@ -10,4 +10,7 @@ Image crop(Image* pic, int x_start, int y_start, int x, int y);
 void make_grey(Image* pic);
 void make_funky(Image* pic, char* pattern);
 void write_image(char* name, Image* pic);
+
+Image make_one(Image* pic1, Image* pic2, int x, int y);
+Image make_image( int x, int y);
 void close_image(Image* i);
